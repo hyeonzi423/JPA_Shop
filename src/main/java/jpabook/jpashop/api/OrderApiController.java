9bfc2d9619fbeec4d471afcd39c4b86ed9433cfc
김현지 <chik1y@naver.com>
2025-02-19 14:16:22 +0900

    주문 조회 V5:JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -69,6 +69,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDto();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     public static class OrderDto {
 
